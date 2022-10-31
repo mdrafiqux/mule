deff15e524ccc975b971797d11fcdc322de23153
rafiqhussain <mohd.rafiqhussain@gmail.com>
2022-10-31 22:45:27 +0530

    json update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import ReactHtmlParser from "react-html-parser";
-import MULEDATA from "./MULE_DATA.json";
+import MULEDATA from "./mule.json";
 import { useState } from "react";
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   return (
     <>
-      {/* <Stories /> */}
       <div className="container">
         <div className="app">
           <h2 className="mainheading">Search for Mule Docs</h2>
@@ -17,27 +16,27 @@ const App = () => {
               setSearchTerm(event.target.value);
             }}
           />
-          {MULEDATA &&
-            MULEDATA.filter((val) => {
-              if (searchTerm === "") {
-                // return val;
-              } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase()) || val.content.toLowerCase().includes(searchTerm.toLowerCase()) || val.points.toLowerCase().includes(searchTerm.toLowerCase())) {
-                return val;
-              }
-            }).map((val, key) => {
-              return (
-                <div className="box" key={val.id}>
-                  <h4 className="heading">{val.title}</h4>
-                  <p className="paragraph">{val.content}</p>
-                  <p className="paragraph">{ReactHtmlParser(val.points)}</p>
-                  <p className="paragraph">{ReactHtmlParser(val.points1)}</p>
-                  <p className="paragraph">{ReactHtmlParser(val.points2)}</p>
-                  <p className="paragraph">{ReactHtmlParser(val.points3)}</p>
-                  <p className="paragraph">{ReactHtmlParser(val.points4)}</p>
-                  <p className="paragraph">{ReactHtmlParser(val.image)}</p>
-                </div>
-              );
-            })}
+          {MULEDATA.filter((val) => {
+            if (searchTerm === "") {
+              // return val;
+            } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase()) || val.content.toLowerCase().includes(searchTerm.toLowerCase()) || val.point1.toLowerCase().includes(searchTerm.toLowerCase()) || val.point2.toLowerCase().includes(searchTerm.toLowerCase()) || val.point3.toLowerCase().includes(searchTerm.toLowerCase()) || val.point4.toLowerCase().includes(searchTerm.toLowerCase()) || val.point5.toLowerCase().includes(searchTerm.toLowerCase()) || val.point6.toLowerCase().includes(searchTerm.toLowerCase())) {
+              return val;
+            }
+          }).map((val, key) => {
+            return (
+              <div className="box" key={val.id}>
+                <h4 className="heading">{val.title}</h4>
+                <p className="paragraph">{val.content}</p>
+                <p className="paragraph">{ReactHtmlParser(val.point1)}</p>
+                <p className="paragraph">{ReactHtmlParser(val.point2)}</p>
+                <p className="paragraph">{ReactHtmlParser(val.point3)}</p>
+                <p className="paragraph">{ReactHtmlParser(val.point4)}</p>
+                <p className="paragraph">{ReactHtmlParser(val.point5)}</p>
+                <p className="paragraph">{ReactHtmlParser(val.point6)}</p>
+                {/* <p className="paragraph">{ReactHtmlParser(val.image)}</p> */}
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
