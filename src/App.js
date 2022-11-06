0343dd8e73c2ee8295c739038e62271ae6abb8f3
mdrafiqux <rafiqux84@gmail.com>
2022-11-06 11:19:22 +0530

    added index

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import ReactHtmlParser from "react-html-parser";
 import MULEDATA from "./questions.json";
 import { useState } from "react";
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [show, setShow] = useState(false);
   return (
     <>
       <div className="container">
@@ -16,6 +18,9 @@ const App = () => {
               setSearchTerm(event.target.value);
             }}
           />
+          <div>
+           <button className="button is-link " type="button" onClick={()=>setShow(!show)}>{show === true ? "Hide Index" : "Show Index"}</button>
+           </div>
           {MULEDATA.filter((val) => {
             if (searchTerm === "") {
               // return val;
@@ -39,6 +44,57 @@ const App = () => {
             );
           })}
         </div>
+      </div>
+      <div className="container pad-lr-20">
+     
+      
+        {
+         MULEDATA.map( (questions,key) => {
+            return(
+              <>
+              <div className="" key={questions.id}>
+                {
+                  show &&  
+                  <>
+                  <div className="">
+                  <ul><li><a href={"#"+questions.id}>{questions.id}. {questions.title}</a></li></ul>
+                  </div>
+                  </>
+                                  }
+              </div>
+              
+              </>
+            )
+          })
+        }
+        {
+         MULEDATA.map( (questionblocks,key) => {
+            return(
+              <>
+              <div className="" key={questionblocks.id}>
+                {
+                  show &&  
+                  <>
+                  <div className="questions-list" id={questionblocks.id}>
+                  <h4 className="heading">{questionblocks.title}</h4>
+                <p className="paragraph">{ReactHtmlParser(questionblocks.content)}</p>
+                <p className="paragraph">{ReactHtmlParser(questionblocks.point1)}</p>
+                <p className="paragraph">{ReactHtmlParser(questionblocks.point2)}</p>
+                <p className="paragraph">{ReactHtmlParser(questionblocks.point3)}</p>
+                <p className="paragraph">{ReactHtmlParser(questionblocks.point4)}</p>
+                <p className="paragraph">{ReactHtmlParser(questionblocks.point5)}</p>
+                <p className="paragraph">{ReactHtmlParser(questionblocks.point6)}</p>
+                <p className="paragraph">{ReactHtmlParser(questionblocks.image)}</p>
+                  </div>
+                  </>
+                                  }
+              </div>
+              
+              </>
+            )
+          })
+        }
+      
       </div>
     </>
   );
